419a30d454765f67e69bd00b83e475d0d76b0193
PrzemekMiros <kontakt@futurewebstudio.pl>
2026-04-27 16:36:34 +0200

    Remove maintenance

--- .eleventy.js
+++ .eleventy.js
@@ -12,7 +12,7 @@ module.exports = function(eleventyConfig) {
   });
 
   // Kopiuj pliki statyczne
-  eleventyConfig.addPassthroughCopy("src/assets");
+  eleventyConfig.addPassthroughCopy("src/assets/");
   eleventyConfig.addPassthroughCopy("src/content/interviews/img");
   eleventyConfig.addPassthroughCopy("src/content/jury/img");
   eleventyConfig.addPassthroughCopy({
@@ -43,6 +43,26 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addGlobalData("placeImages", () => {
     const placeDir = path.join(__dirname, "src/content/place/img");
     const allowedExt = new Set([".jpg", ".jpeg", ".png", ".webp", ".avif"]);
+    const placeAltsByFile = {
+      "487455428_1088427919995603_3029706944664290231_n.jpg": "Gala International Businesswoman Awards w reprezentacyjnych wnętrzach w Rzymie",
+      "BRANCACCIO-PARK_.jpg": "Ogród Palazzo Brancaccio w Rzymie",
+      "BRANCACCIO.jpg": "Pałac Brancaccio w Rzymie",
+      "BrancaccioPOP.jpg": "Detale architektury Palazzo Brancaccio",
+      "BRANCACCIO_FOTO.jpg": "Historyczne wnętrze Palazzo Brancaccio",
+      "BRANCACCIO_POPPOP-1.jpeg": "Sala bankietowa Palazzo Brancaccio",
+      "BWA_ROMA_0002-scaled.jpg": "Goście gali International Businesswoman Awards w Rzymie",
+      "BWA_ROMA_0007-scaled.jpg": "Scena wydarzenia International Businesswoman Awards",
+      "BWA_ROMA_0594-scaled.jpg": "Uroczysty moment gali International Businesswoman Awards",
+      "otoczenie.jpg": "Otoczenie i zabudowa historycznej części Rzymu",
+      "palazzo-brancaccio-verticale.jpeg": "Fasada Palazzo Brancaccio w pionowym kadrze",
+      "Palazzo-Brancaccio-_.jpeg": "Front Palazzo Brancaccio",
+      "Palazzo-Brancaccio_1-1.jpg": "Wejście do Palazzo Brancaccio",
+      "park__.jpg": "Park przy Palazzo Brancaccio",
+      "rome-palazzo-brancaccio-preview-7509.jpg": "Widok Palazzo Brancaccio w Rzymie",
+      "roof-garden-hotel-forum-restaurant-083-min.jpg": "Roof Garden Hotel Forum w Rzymie",
+      "SALA-BRANCACCIO.jpg": "Sala Pałacu Brancaccio przygotowana na galę",
+      "statuetki-scaled.jpg": "Statuetki International Businesswoman Awards"
+    };
 
     if (!fs.existsSync(placeDir)) {
       return [];
@@ -56,7 +76,7 @@ module.exports = function(eleventyConfig) {
       .sort((a, b) => a.localeCompare(b, "pl", { sensitivity: "base" }))
       .map((name) => ({
         src: `/assets/place/${encodeURIComponent(name)}`,
-        alt: path.parse(name).name.replace(/[-_]+/g, " ")
+        alt: placeAltsByFile[name] || `Miejsce gali International Businesswoman Awards - ${path.parse(name).name.replace(/[-_]+/g, " ")}`
       }));
   });
 
